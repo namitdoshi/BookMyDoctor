7946fbaf9a85a2d06faa80637ce5f639040e3ad9
Namit Doshi <namitdoshi90@gmail.com>
2019-03-18 20:35:47 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Login from './src/components/Login';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Login />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
+  },
+});
